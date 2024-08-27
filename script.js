3957f863d258b2cd787350e87c75dd3f4a80f368
manuel <manuher1229@hotmail.com>
2024-08-27 05:08:27 -0400

    Files Created, basic content added.

---
+++ script.js
@@ -0,0 +1,18 @@
+function Gameboard() {
+    const rows = 2;
+    const columns = 2;
+    const board = [];
+}
+
+
+function makeMove() {
+}
+
+function checkWin() {
+}
+
+function checkDraw() {
+}
+
+function startGame() {
+}
